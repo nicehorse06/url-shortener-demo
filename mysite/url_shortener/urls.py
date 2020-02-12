fd13053039b030fd81ca1103d89fd7c692ee2e65
jimmyma <jimmyma@croxlink.com>
2020-02-12 11:47:58 +0800

    完成簡易縮網址實驗

--- mysite/url_shortener/urls.py
+++ mysite/url_shortener/urls.py
@@ -3,5 +3,6 @@ from .views import ShortenerView
 
 app_name = 'shortener'
 urlpatterns = [
+	path('<int:url_id>/', ShortenerView.as_view()),
     path('', ShortenerView.as_view(), name='shortener'),
 ]
--- mysite/url_shortener/views.py
+++ mysite/url_shortener/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from django.views import View
 from .models import UrlRecord
 from .forms import UrlRecordForm
+from django.shortcuts import redirect
 
 # Create your views here.
 class ShortenerView(View):
-	def get(self, request):
+	def get(self, request, **kwargs):
+		if kwargs.get('url_id'):
+			# 確認到轉址URL
+			return redirect('https://www.google.com/')
 		return render(request, 'index.html', {'UrlRecordForm': UrlRecordForm()})
 	def post(self, request):
 		form = UrlRecordForm(request.POST)
@@ -14,5 +18,5 @@ class ShortenerView(View):
 			this_url_record = UrlRecord(origin_url=cleaned_data['origin_url'])
 			this_url_record.save()
 			# todo要回傳新網址
-
+			# todo 要顯示轉網址結果
 		return self.get(request)
