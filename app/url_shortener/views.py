9564dfa3e72a0b7957b7d20a20d08467a0484922
jimmyma <jimmyma@croxlink.com>
2020-02-13 18:19:18 +0800

    檢查錯誤參數

--- app/url_shortener/views.py
+++ app/url_shortener/views.py
@@ -11,13 +11,18 @@ class ShortenerView(View):
 	def get(self, request, info='', **kwargs):
 		key = kwargs.get('key')
 		if key:
-			# 確認到轉址URL
-			this_url_record = UrlRecord.objects.filter(id=key).first()
-			if this_url_record:
-				return redirect(this_url_record.origin_url)
-			else:
-				info = '%s為無效的縮網址' % UrlRecord.shortener_url(key)
+			info = '%s 為無效的縮網址' % UrlRecord.shortener_url(key)
+
+			try: 
+				this_url_record = UrlRecord.objects.get(id=key)
+				if this_url_record:
+					info = ''
+					return redirect(this_url_record.origin_url)
+			except:
+				pass
+
 		return render(request, 'index.html', {'UrlRecordForm': UrlRecordForm(), 'info': info})
+
 	def post(self, request):
 		form = UrlRecordForm(request.POST)
 		info = ''
@@ -26,7 +31,7 @@ class ShortenerView(View):
 			cleaned_data = form.cleaned_data
 			this_url_record = UrlRecord(origin_url=cleaned_data['origin_url'])
 			this_url_record.save()
-			info = '%s\n新的短網址為\n%s' % (this_url_record.origin_url, UrlRecord.shortener_url(this_url_record.id))
+			info = '%s\n 新的短網址為 \n%s' % (this_url_record.origin_url, UrlRecord.shortener_url(this_url_record.id))
 		else:
 			info = '網址的格式不正確!'
 
